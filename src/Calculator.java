81433236f4871794217c18cabaa969394911bd93
kimyujoon <kimyujoon@172.20.10.4>
2017-07-07 23:27:34 +0900

    initial commit2

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,5 +3,8 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("덧셈:" + (3 + 4));
+		System.out.println("뺄셈:" + (3 + 4));
+		System.out.println("곱셈:" + (3 + 4));
+		System.out.println("나눗셈:" + (3 + 4));
 	}
 }
