ce029616e8ccb12e7180b3c976f75a373e628421
kimyujoon <kimyujoon@172.20.10.3>
2017-07-27 22:04:12 +0900

    두개의 값을 입력받아서 계

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("덧셈:" + (3 + 4));
-		System.out.println("뺄셈:" + (3 + 4));
-		System.out.println("곱셈:" + (3 + 4));
-		System.out.println("나눗셈:" + (3 + 4));
+		System.out.println("첫번째 값을 입력해 주세요:");
+		Scanner scanner = new Scanner(System.in);
+		int first = scanner.nextInt();
+		System.out.println(first);
+		System.out.println("두번째 값을 입력해 주세요:");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		System.out.println("=>"+ first+" * "+second +" :" + (first*second));
+		
 	}
 }
