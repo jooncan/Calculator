da52877a0f1048c98aca175af7a359afb582fb9a
kimyujoon <kimyujoon@172.20.10.3>
2017-07-27 22:44:50 +0900

    계산기 기능 개발 완

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,10 +7,42 @@ public class Calculator {
 		Scanner scanner = new Scanner(System.in);
 		int first = scanner.nextInt();
 		System.out.println(first);
-		System.out.println("두번째 값을 입력해 주세요:");
-		int second = scanner.nextInt();
-		System.out.println(second);
-		System.out.println("=>"+ first+" * "+second +" :" + (first*second));
 		
+		int result = first;
+		while(true) {
+			
+			System.out.println("연산 입력해 주세요:");
+			String symbol = scanner.next();
+			System.out.println(symbol);
+			
+			if(symbol.equals("quit")) {
+				System.out.println("최종결과 값은: " + result);
+				break;
+			}
+			
+			System.out.println("두번째 값을 입력해 주세요:");
+			int second = scanner.nextInt();
+
+			if(symbol.equals("+")) {
+				result = result + second;
+				System.out.println("연산결과:"+ result);
+			}
+			else if(symbol.equals("-")) {
+				result = result - second;
+				System.out.println("연산결과:"+ result);
+			}
+			else if(symbol.equals("*")) {
+				result = result * second;
+				System.out.println("연산결과:"+ result);
+			}
+			else if(symbol.equals("/")) {
+				result = result /second;
+				System.out.println("연산결과:"+ result);
+			}
+			else {
+				System.out.println("연산자가 아닙니다");
+			}
+			
+		}
 	}
 }
