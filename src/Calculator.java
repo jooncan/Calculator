22a25f7116fecd476002a7bd880a4783fa2c9b3c
kimyujoon <kimyujoon@172.20.10.3>
2017-07-27 23:08:41 +0900

    메쏘드 형식으로 변

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,48 +1,70 @@
 import java.util.Scanner;
 
 public class Calculator {
+	
+	public static int getfirstvalue(Scanner scanner) {
+		System.out.println("첫번째 값을 입력해 주세요:");
+		return scanner.nextInt();
+	}
+
+	public static String getsymbol(Scanner scanner) {
+		System.out.println("연산자 입력해 주세요:");
+		return scanner.next();
+	}
+
+	public static int getsecondvalue(Scanner scanner) {
+		System.out.println("두번째 값을 입력해 주세요:");
+		return scanner.nextInt();
+	}
+	
+	public static int calculate(int first, String symbol, int second){
+		int result = 0;
+		if(symbol.equals("+")) {
+			result = first + second;
+		}
+		else if(symbol.equals("-")) {
+			result = first - second;
+		}
+		else if(symbol.equals("*")) {
+			result = first * second;
+		}
+		else if(symbol.equals("/")) {
+			result = first / second;
+		}
+		else {
+			System.out.println("연산자가 아닙니다");
+			result = first;
+		}
+		
+		return result;
+		
+	}
+	
+	public static void print(int result) {
+		System.out.println("연산 결과는 :"+ result);
+	}
+	
 
 	public static void main(String[] args) {
-		System.out.println("첫번째 값을 입력해 주세요:");
 		Scanner scanner = new Scanner(System.in);
-		int first = scanner.nextInt();
-		System.out.println(first);
+
+		int first = getfirstvalue(scanner);
 		
 		int result = first;
 		while(true) {
 			
-			System.out.println("연산 입력해 주세요:");
-			String symbol = scanner.next();
-			System.out.println(symbol);
+			String symbol = getsymbol(scanner);
 			
 			if(symbol.equals("quit")) {
 				System.out.println("최종결과 값은: " + result);
 				break;
 			}
 			
-			System.out.println("두번째 값을 입력해 주세요:");
-			int second = scanner.nextInt();
+			int second = getsecondvalue(scanner);
 
-			if(symbol.equals("+")) {
-				result = result + second;
-				System.out.println("연산결과:"+ result);
-			}
-			else if(symbol.equals("-")) {
-				result = result - second;
-				System.out.println("연산결과:"+ result);
-			}
-			else if(symbol.equals("*")) {
-				result = result * second;
-				System.out.println("연산결과:"+ result);
-			}
-			else if(symbol.equals("/")) {
-				result = result /second;
-				System.out.println("연산결과:"+ result);
-			}
-			else {
-				System.out.println("연산자가 아닙니다");
-			}
+			result = calculate(result, symbol, second);
 			
+			print(result);			
 		}
 	}
 }
